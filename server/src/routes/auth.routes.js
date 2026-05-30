dcf32ee1801247f41bccc69a91c8d123b5825370
= <ybharambe591@gmail.com>
2026-05-30 10:55:25 +0530

    Auth route update.

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const authMiddleware = require("../middleware/auth.middleware");
-const { register, login, me } = require("../controllers/auth.controller");
+const { register, login, me, updateProfile } = require("../controllers/auth.controller");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", authMiddleware, me);
+router.patch("/profile", authMiddleware, updateProfile);
 
 module.exports = router;
